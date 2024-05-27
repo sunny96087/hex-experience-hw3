f1b6d8797420d5854b1e19022f559f9f672fd73d
紀博穎 <jiboying@MacBook-Pro.local>
2024-05-27 20:07:16 +0800

    5/27 update css

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
     // 當銀幕小於768px時 將導覽列class加入close 按下按鈕toggle
     function checkScreenSize() {
       if ($(window).width() < 768) {
-         $(".nav-bar-list").addClass("close"); 
+         $(".nav-bar-list").addClass("close");
+         $(".hamburger").removeClass("close"); 
+         $(".closure").addClass("close");
       } else {
         $(".nav-bar-list").removeClass("close"); 
       }
@@ -17,6 +19,8 @@ $(document).ready(function(){
 
     $(".navbar-btn").click(function(){
         $(".nav-bar-list").toggleClass("close");
+        $(".hamburger").toggleClass("close");
+        $(".closure").toggleClass("close");
       });
 
  });
